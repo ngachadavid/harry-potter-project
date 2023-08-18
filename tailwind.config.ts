66dcf04ee9d91b58c53d715234284691d87de7dd
David <davidngacha01@gmail.com>
2023-08-18 12:08:50 +0300

    modified:   tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        gryffindor: '#7F0909',
+        slytherin: '#1A472A',
+        hufflepuff: '#DAA520',
+        ravenclaw: '#0E1A40',
+      },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
