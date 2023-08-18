d464e076716e2342ea8b5b636dce705b9c92b245
David <davidngacha01@gmail.com>
2023-08-18 12:22:53 +0300

    fix: add commentings

--- pages/characterId.tsx
+++ pages/characterId.tsx
@@ -13,30 +13,36 @@ interface Character {
     length: number;
   };
   patronus: string;
-  // Add other properties as needed
 }
 
 const CharacterDetail: React.FC = () => {
+  // Get the route parameters using useRouter
   const router = useRouter();
   const { id } = router.query;
+
+  // State to store character data
   const [character, setCharacter] = useState<Character | null>(null);
 
   useEffect(() => {
+    // Fetch character data when the component mounts
     const fetchCharacter = async () => {
       try {
         if (id) {
           console.log('Fetching character details for ID:', id);
-          
+
+          // Fetch character data from the API
           const response = await axios.get(`https://hp-api.onrender.com/api/character/${id}`);
           console.log('API Response:', response.data);
-          
+
+          // Update character state with fetched data
           setCharacter(response.data);
         }
       } catch (error) {
         console.error('Error fetching character details:', error);
       }
     };
-    
+
+    // Check if the id is available and fetch the character
     if (id) {
       console.log('Component mounted with ID:', id);
       fetchCharacter();
@@ -46,6 +52,7 @@ const CharacterDetail: React.FC = () => {
   return (
     <div className="p-4">
       {character ? (
+        // Display character details
         <div className="border p-4 rounded-lg shadow-md bg-blue-100">
           <h2 className="text-2xl font-semibold mb-2">{character.name}</h2>
           <p className="text-gray-600 mb-1">House: {character.house}</p>
@@ -54,9 +61,9 @@ const CharacterDetail: React.FC = () => {
             Wand: {character.wand.wood} wood {character.wand.core}, {character.wand.length}
           </p>
           <p className="text-gray-600 mb-4">Patronus: {character.patronus}</p>
-          {/* Add more character details */}
         </div>
       ) : (
+        // Display loading message if character data is not available yet
         <p className="text-center">Loading character details...</p>
       )}
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-
-// interface Character {
-//   id: string;
-//   name: string;
-//   house: string;
-//   dateOfBirth: string;
-//   wand: {
-//     wood: string;
-//     core: string;
-//     length: number;
-//   };
-//   patronus: string;
-//   // Add other properties as needed
-// }
-
-// const Home: React.FC = () => {
-//   const [characters, setCharacters] = useState<Character[]>([]);
-//   const [searchQuery, setSearchQuery] = useState('');
-
-//   useEffect(() => {
-//     const fetchCharacters = async () => {
-//       try {
-//         const response = await axios.get('https://hp-api.onrender.com/api/characters');
-//         setCharacters(response.data);
-//       } catch (error) {
-//         console.error('Error fetching characters:', error);
-//       }
-//     };
-//     fetchCharacters();
-//   }, []);
-
-//   const filteredCharacters = characters.filter((character) =>
-//     character.name.toLowerCase().includes(searchQuery.toLowerCase())
-//   );
-
-//   return (
-//     <div className="p-4">
-//       <input
-//         type="text"
-//         placeholder="Search by name"
-//         className="w-full p-2 mb-4 border rounded"
-//         value={searchQuery}
-//         onChange={(e) => setSearchQuery(e.target.value)}
-//       />
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-//         {filteredCharacters.map((character) => (
-//           <div key={character.id} className="p-4 bg-white rounded shadow">
-//             <div className="border p-4 rounded-lg shadow">
-//               <h2 className="text-xl font-semibold mb-2">{character.name}</h2>
-//               <p>House: {character.house}</p>
-//               <p>Date of Birth: {character.dateOfBirth}</p>
-//               <p>Wand: {character.wand.wood} wood, {character.wand.core}, {character.wand.length}"</p>
-//               <p>Patronus: {character.patronus}</p>
-//               {/* Add more character details */}
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -78,35 +12,42 @@ interface Character {
     length: number;
   };
   patronus: string;
-  // Add other properties as needed
 }
 
 const Home: React.FC = () => {
+  // State to store the list of characters
   const [characters, setCharacters] = useState<Character[]>([]);
+  // State to store the search query
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
+    // Fetch characters from the API when the component mounts
     const fetchCharacters = async () => {
       try {
+        // Fetch characters data from the API
         const response = await axios.get('https://hp-api.onrender.com/api/characters');
         setCharacters(response.data);
       } catch (error) {
         console.error('Error fetching characters:', error);
       }
     };
+    // Call the fetchCharacters function
     fetchCharacters();
   }, []);
 
+  // Event handler for handling search input changes
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   };
 
+  // Filter characters based on the search query
   const filteredCharacters = characters.filter((character) =>
     character.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
     <div className="p-4">
+      {/* Search input */}
       <input
         type="text"
         placeholder="Search by name"
@@ -115,6 +56,7 @@ const Home: React.FC = () => {
         onChange={handleSearchChange}
       />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {/* Render characters */}
         {filteredCharacters.map((character) => (
           <div key={character.id} className="p-4 bg-white rounded shadow">
             <div className="border p-4 rounded-lg shadow">
@@ -125,7 +67,6 @@ const Home: React.FC = () => {
                 Wand: {character.wand.wood} wood, {character.wand.core}, {character.wand.length}
               </p>
               <p>Patronus: {character.patronus}</p>
-              {/* Add more character details */}
             </div>
           </div>
         ))}
@@ -135,4 +76,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
